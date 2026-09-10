195a5fef120d3e419d0048569cf54806e981dfc9
vytalz <shreyassingh121@gmail.com>
2026-09-10 16:01:00 -0400

    Should include teleop example

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MotorTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MotorTestOpMode.java
@@ -1,4 +1,52 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-public class MotorTestOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
+
+@TeleOp
+public class MotorTestOpMode extends CommandOpMode {
+    GamepadEx driverPad;
+
+    private DcMotorEx testMotor;
+
+    public void initialize() {
+
+        driverPad = new GamepadEx(gamepad1);
+        testMotor = hardwareMap.get(DcMotorEx.class, "testmotor");
+
+
+
+
+    }
+
+    public void run() {
+
+        if (driverPad.getButton(GamepadKeys.Button.X)) {
+
+            testMotor.setPower(1);
+
+
+
+        } else {
+
+            testMotor.setPower(0);
+        }
+
+        if (driverPad.getButton(GamepadKeys.Button.A)) {
+
+            testMotor.setPower(-1);
+
+
+
+        } else {
+
+            testMotor.setPower(0);
+        }
+
+
+    }
 }
