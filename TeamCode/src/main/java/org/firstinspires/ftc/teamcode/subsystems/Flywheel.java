cba131fb63bdcc46a0356c3ea131782dbea0a620
vytalz <shreyassingh121@gmail.com>
2026-09-12 15:30:51 -0400

    Flywheel framework laid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Flywheel.java
@@ -5,9 +5,15 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.seattlesolvers.solverslib.controller.PIDController;
+import com.seattlesolvers.solverslib.controller.PIDFController;
 
 public class Flywheel extends SubsystemBase {
     private  DcMotorEx flywheel1, flywheel2;
+
+    private PIDController flywheelpid;
+
+    private double currentVelo, targetVelo, output;
 
 
 
@@ -16,25 +22,61 @@ public class Flywheel extends SubsystemBase {
         flywheel1 = hMap.get(DcMotorEx.class, "fly1");
         flywheel2 = hMap.get(DcMotorEx.class, "fly2");
 
-        flywheel1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        flywheel2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        flywheel1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheel2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         flywheel1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         flywheel2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
+        flywheelpid = new PIDController(0,0,0);
+
 
 
     }
+
+    public double velocityGetter() {
+
+        currentVelo = flywheel1.getVelocity();
+        return currentVelo;
+
+    }
+
+    public void setTargetVelo(double tVelo) {
+        targetVelo = tVelo;
+
+    }
+
+    public double targetVeloGetter() {
+    return targetVelo;
+
+    }
+
+
+
+
+
+    public void setPID(double kP, double kI, double kD) {
+
+        flywheelpid.setPID(kP, kI, kD);
+
+
+
+    }
+
+
 
     @Override
-
-
-
     public void periodic() {
+
+        currentVelo = flywheel1.getVelocity();
+
+
+        output = flywheelpid.calculate(currentVelo,targetVelo);
+
+        flywheel1.setPower(output);
 
 
     }
-
 
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/FlywheelPIDTuningOpmode.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+
+
+import org.firstinspires.ftc.teamcode.subsystems.Flywheel;
+
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Config
+@TeleOp(name = "Flywheel PID Tuning Opmode")
+public class FlywheelPIDTuningOpmode extends CommandOpMode {
+
+    private Flywheel flywheel;
+
+    private FtcDashboard dash;
+
+    private GamepadEx driverPad;
+
+
+    public static double kP = 0.000455; // Default PID constants
+    public static double kI = 0.0000000;
+    public static double kD = 0.00000;
+
+    public static double pidTarget = 0;
+
+    @Override
+    public void initialize() {
+        // Initialize the Spindex subsystem
+
+        driverPad = new GamepadEx(gamepad1);
+        dash = FtcDashboard.getInstance();
+        flywheel = new Flywheel(hardwareMap);
+
+
+        // Register the Spindex subsystem
+        register(flywheel);
+
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+
+
+
+
+
+        // Read the PID constants from the Dashboard sliders
+        // Assuming these sliders are created on the Dashboard with the names "kP", "kI", "kD"// Default to current value if not set
+
+        // Set the updated PID values in the Spindex subsystem
+        flywheel.setPID(kP, kI, kD);
+
+        // Get the current position and the target position
+        double currentVelo = flywheel.velocityGetter();
+        pidTarget = flywheel.targetVeloGetter();
+
+        // Calculate the PID error
+        double error = Math.abs(pidTarget) - Math.abs(currentVelo);
+
+        // Send the PID constants and the error to the telemetry
+
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.put("PID kP", kP);
+        packet.put("PID kI", kI);
+        packet.put("PID kD", kD);
+        packet.put("Current Velo(Ticks)", currentVelo);
+        packet.put("Target Velo (Ticks)", pidTarget);
+        packet.put("Error", error);
+        dash.sendTelemetryPacket(packet);
+
+    }
+}
